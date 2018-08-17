82cf2e404a9816052f7ce9d1f6784f07525e7e59
Xiangzhi Liang <xiang_zhi_@126.com>
2018-08-17 16:34:36 +0800

    Update example.sh

--- test/example.sh
+++ test/example.sh
@@ -1,3 +1,3 @@
-python json2csv/gen_outline.py microbe.oxygen.json -e -o outline.json
+python json2csv/gen_outline.py test.data.json -e -o outline.json
 
-python json2csv/json2csv.py microbe.oxygen.json outline.json -e -o microbe.oxygen.csv
+python json2csv/json2csv.py test.data.json outline.json -e -o out.csv
